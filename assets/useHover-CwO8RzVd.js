81c33d5dc5777234842ee50c7466529a91128a00
QuanNV96 <quannv@icorp.com.vn>
2026-05-30 23:51:07 +0700

    build

---
+++ assets/useHover-CwO8RzVd.js
@@ -0,0 +1,1 @@
+import{p as t,w as u}from"./index-CQB6AARS.js";function l(){const[n,s]=t.useState(!1),e=t.useRef(null),o=()=>s(!0),a=()=>s(!1);return u("mouseenter",o,e),u("mouseleave",a,e),[e,n]}export{l as u};
