f81899d35ae2dd3519d0ade62d54177b5e6889b2
QuanNguyen96 <89249444+QuanNguyen96@users.noreply.github.com>
2026-05-31 01:35:00 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths'
 import eslint from 'vite-plugin-eslint'
@@ -6,13 +6,17 @@ import svgr from 'vite-plugin-svgr'
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
-export default defineConfig({
-  base: "/vestia/",
-  plugins: [
-    react(),
-    jsconfigPaths(),
-    svgr(),
-    eslint(),
-    tailwindcss(),
-  ],
-})
+
+export default defineConfig((mode) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env?.BASE_URL || '',
+    plugins: [
+      react(),
+      jsconfigPaths(),
+      svgr(),
+      eslint(),
+      tailwindcss(),
+    ],
+  };
+});
